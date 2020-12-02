4586a0efea1c32ae21eb68a06b01e9ff646179f2
stevenacz <scoailazaa@gmail.com>
2020-12-02 01:58:48 -0500

    Problem 03

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -6,3 +6,12 @@ To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
 
+class ResistorColorDuo
+  COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+  def self.value(arr_duo_resistor)
+    first = COLORS.find_index { |color| color == arr_duo_resistor[0] }
+    second = COLORS.find_index { |color| color == arr_duo_resistor[1] }
+    "#{first}#{second}".to_i
+  end
+end
