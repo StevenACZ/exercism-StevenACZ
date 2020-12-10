aad5d4b0b5d5692c2e77809e84fa1b6d8a406890
stevenacz <scoailazaa@gmail.com>
2020-12-10 16:03:21 -0500

    Problem 07

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -8,20 +8,13 @@ To get started with TDD, see the `README.md` file in your
 
 class Series
   def initialize(string_num)
-    @string_num = string_num.split("")
+    @string_num = string_num.chars
   end
 
   def slices(n_times)
-    raise ArgumentError if n_times > @string_num.size
-    
-    res = []
-    i = 0
-
-    while i + n_times <= @string_num.size
-      res.push(@string_num[i, n_times].join)
-      i += 1
-    end
-
-    res
+    raise ArgumentError unless n_times <= @string_num.size
+    series = []
+    @string_num.each_cons(n_times) { |serie| series.push serie.join }
+    series
   end
 end
