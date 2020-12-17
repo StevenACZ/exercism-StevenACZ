8e66c89cbf254d4d07b934bd340422f28a28ee05
stevenacz <scoailazaa@gmail.com>
2020-12-16 21:04:37 -0500

    Problem 09

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,4 @@
-=begin
-Write your code for the 'Hamming' exercise in this file. Make the tests in
-`hamming_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/hamming` directory.
-=end
-
+# Hamming class has a method that calculate the difference between two DNA strands.
 class Hamming
   # compute function returns the count of different threads, as long as
   #   they have the same size, otherwise it will raise an ArgumentError
