bb6232e4c3d595ad714975127cf1219ddde21490
stevenacz <scoailazaa@gmail.com>
2020-12-27 18:58:39 -0500

    Problem 10

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,14 +1,14 @@
 # Raindrops class has a method that convert a number to a string
 class Raindrops
-  # convert function convert a number to a string,
+  # convert methods convert a number to a string,
   #   the contents of which depend on the number's factors.
-  def self.convert(number)
-    result = ""
+  def self.convert(integer)
+    sound = ""
     
-    result += "Pling" if number % 3 == 0
-    result += "Plang" if number % 5 == 0
-    result += "Plong" if number % 7 == 0
+    sound << "Pling" if integer % 3 == 0
+    sound << "Plang" if integer % 5 == 0
+    sound << "Plong" if integer % 7 == 0
 
-    result.empty? ? number.to_s : result
+    sound.empty? ? "#{integer}" : sound
   end
 end
