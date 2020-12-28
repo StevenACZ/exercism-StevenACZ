0ac8100efc58440256ae7dbf026acf19fe59a9b3
stevenacz <scoailazaa@gmail.com>
2020-12-28 00:59:29 -0500

    Problem 03 Sub 01

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,11 +1,3 @@
-=begin
-Write your code for the 'Leap' exercise in this file. Make the tests in
-`leap_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/leap` directory.
-=end
-
 class Year
   def self.leap?(year)
     return year.divisible_by?(100) ? year.divisible_by?(400) : year.divisible_by?(4)
